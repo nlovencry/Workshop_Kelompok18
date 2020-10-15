4b455ac5492fc6b47a94c6c9e4c524f77c28c328
athfalkamali <72002107+athfalkamali@users.noreply.github.com>
2020-10-15 15:51:37 +0700

    Add files via upload

---
+++ E41190892_ATHFAL_KAMALI/tipe-data.php
@@ -0,0 +1,20 @@
+<?php 
+$tes = "Saya Pergi Kerja";
+// variabel tes di atas merupakan tipe data string karena berisi text atau kalimat.
+$bilangan_pertama = 12;
+$bilangan_kedua = 78;
+//Kedua variabel di atas merupakan variabel yang bertipe data integer berisi angka. 
+$angka = 12.177;
+//variabel di atas adalah variabel yang bertipe data float karena berisi bilangan desimal.
+$x = false;
+$y = true;
+//variabel di atas bertipe data boolean karena berisi nilai benar atau salah.
+$anggota = array("Andi","Budi","Joni");
+//variabel anggota di atas adalah variabel yang bertipe data array karena memiliki banyak isi.
+
+echo "tipe data string: ".$tes."<br>";
+echo "tipe data integer: ".$bilangan_pertama." , ".$bilangan_kedua."<br>";
+echo "tipe data float: ".$angka."<br>";
+echo "tipe data boolean: ".$x,$y."<br>";
+echo "tipe data array: ".$anggota[0],$anggota[1],$anggota[2]
+?>
