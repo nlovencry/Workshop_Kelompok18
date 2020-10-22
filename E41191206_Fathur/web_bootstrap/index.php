5b4580ed6828191d8cd403a247445737facf7c9c
Fathur Rohman <fathur.rahman12300@gmail.com>
2020-10-22 19:09:29 +0700

    update

--- E41191206_Fathur/web_bootstrap/index.php
+++ E41191206_Fathur/web_bootstrap/index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Workshop - Dashboard</title>
+  <title>Workshop</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
