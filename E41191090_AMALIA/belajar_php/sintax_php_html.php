1b5a233002a11571d8c089f7364f9a7ac7696a0f
Amaliafcn <71832956+Amaliafcn@users.noreply.github.com>
2020-10-29 22:23:41 +0700

    update

--- E41191090_AMALIA/belajar_php/sintax_php_html.php
+++ E41191090_AMALIA/belajar_php/sintax_php_html.php
@@ -5,7 +5,7 @@
     echo "<title>Judul Web</title>";
     echo "</head>";
     echo "<body>";
-    echo "<h1>selamat Datang</h1>";
+    echo "<h1>Selamat Datang</h1>";
     echo "</body>";
     echo "</html>";
 
