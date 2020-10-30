e3b69808f5513c4c09e0e662243c53286f8772c7
athfalkamali <athfalkamali22@gmail.com>
2020-10-30 18:00:57 +0700

    TUGAS

---
+++ E41190892_ATHFAL_KAMALI/praktikum_6/constanta.php
@@ -0,0 +1,10 @@
+<?php
+//membuat konstanta
+defline('VERSION','1.0.0');
+const SITE_NAME="Petanikode";
+const BASE_URL="https://www.petanikode.com";
+//cetak hasil konstanta
+echo "Site name : ". SITE_NAME ."<br/>";
+echo "URL : ". BASE_URL ."<br/>";
+echo "Version : ". VERSION ."<br/>";
+?>
