cc2b8877964100c81b91b9a17312f450e1b2f41c
bungakim <72001593+bungakim@users.noreply.github.com>
2020-11-26 13:28:39 +0700

    update

--- E41191322_BUNGA/praktikum10/index.php
+++ E41191322_BUNGA/praktikum10/index.php
@@ -2,59 +2,58 @@
 <html>
 <head>
 	<title>Membuat CRUD dengan PHP dan MySQL - Menampilkan Data dari Database</title>
-	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="judul">
 		<h1>Membuat CRUD dengan PHP MySQL</h1>
 		<h2>Menampilkan Data dari Database</h2>
 	</div>
-	<br>
 	<?php 
 		if (isset($_GET['pesan'])) {
 			$pesan = $_GET['pesan'];
 			if ($pesan == "input") {
-				echo "Data Berhasil di Input";
+				echo "<script>alert('Data Berhasil di Input')</script>";
 			}elseif($pesan == 'update') {
-				echo "Data Berhasil di Update";
+				echo "<script>alert('Data Berhasil di Update')</script>";
 			}elseif ($pesan == 'hapus') {
-				echo "Data Berhasil di Hapus";
+				echo "<script>alert('Data Berhasil di Hapus')</script>";
 			}
 		}
 	?>
-	<br>
+	<div class="container">
 	<a href="input.php" class="tombol"> +Tambah Data</a>
-
-	<h3>Data User</h3>
-	<table border="1">
-		<tr>
-			<th>No</th>
-			<th>Nama</th>
-			<th>Alamat</th>
-			<th>Pekerjaan</th>
-			<th>Opsi</th>
-		</tr>
-		<tr>
-			<?php
-			include 'koneksi.php';
-			$query = mysqli_query($db, "SELECT * FROM tb_user");
-			$nomor = 1;
-			while ($data = mysqli_fetch_array($query)) {
-			?>
-				<tr>
-					<td><?php echo $nomor++; ?></td>
-					<td><?php echo $data['nama_user']; ?></td>
-					<td><?php echo $data['alamat_user']; ?></td>
-					<td><?php echo $data['pekerjaan_user']; ?></td>
-					<td>
-						<a href="edit.php?id_user<?php echo $data['id_user']; ?>" class="edit">Edit</a>
-						<a href="hapus.php?id_user<?php echo $data['id_user']; ?>" class="hapus">Hapus</a>
-					</td>
-				</tr>
-			<?php
-			}
-			?>
-		</tr>
-	</table>
+		<h3>Data User</h3>
+		<table border="1" class="table">
+			<tr>
+				<th>No</th>
+				<th>Nama</th>
+				<th>Alamat</th>
+				<th>Pekerjaan</th>
+				<th>Opsi</th>
+			</tr>
+			<tr>
+				<?php
+				include 'koneksi.php';
+				$query = mysqli_query($db, "SELECT * FROM tb_user");
+				$nomor = 1;
+				while ($data = mysqli_fetch_array($query)) {
+				?>
+					<tr>
+						<td><?php echo $nomor++; ?></td>
+						<td><?php echo $data['nama']; ?></td>
+						<td><?php echo $data['alamat']; ?></td>
+						<td><?php echo $data['pekerjaan']; ?></td>
+						<td align="center">
+							<a href="edit.php?id=<?php echo $data['id']; ?>" class="edit">Edit</a>
+							<a href="hapus.php?id=<?php echo $data['id']; ?>" class="hapus">Hapus</a>
+						</td>
+					</tr>
+				<?php
+				}
+				?>
+			</tr>
+		</table>
+	</div>
 </body>
 </html>
