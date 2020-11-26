714056412e5ea8fab3bd20e5ca804757d22e9193
Amaliafcn <71832956+Amaliafcn@users.noreply.github.com>
2020-11-26 20:46:10 +0700

    update

---
+++ E41191090_AMALIA/crud_php/koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+
+$koneksi = mysqli_connect ("localhost","root","","crud_web");
+
+    if (mysqli_connect_error()){
+    echo "Koneksi Database Gagal : " . mysqli_connect_error ();
+    }
+
+?>
