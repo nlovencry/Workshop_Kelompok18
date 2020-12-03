dea11ce560ab8fe53452ddd397113d53d01e130b
bungakim <72001593+bungakim@users.noreply.github.com>
2020-12-03 21:08:51 +0700

    update praktikum

--- E41191322_BUNGA/web_bootstrap/login.php
+++ E41191322_BUNGA/web_bootstrap/login.php
@@ -33,18 +33,36 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block"></div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+                  <?php
+                  if (isset($_GET['pesan'])) {
+                    $pesan = $_GET['pesan'];
+                    if ($pesan == "emailsalah") {
+                      ?>
+                      <div class="alert alert-danger">
+                        <strong>Danger!</strong> Anda Gagal Login, Email tidak ditemukan.
+                      </div>
+                      <?php
+                    }elseif ($pesan == "passwordsalah") {
+                      ?>
+                      <div class="alert alert-danger">
+                        <strong>Danger!</strong> Anda Gagal Login, Password Salah.
+                      </div>
+                      <?php
+                    }
+                  }
+                  ?>
+                  <form class="user" method="POST" action="login-proses.php">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." required="">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" required="">
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
@@ -52,9 +70,7 @@
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
+                    <button type="submit" class="btn btn-primary btn-user btn-block" name="submit">Login</button>
                     <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
