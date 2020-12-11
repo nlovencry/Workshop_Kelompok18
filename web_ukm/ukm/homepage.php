99dc15847697cd0b1b5c8f5ff52507d49fc27681
bungakim <72001593+bungakim@users.noreply.github.com>
2020-12-11 15:02:33 +0700

    Update homepage.php

--- web_ukm/ukm/homepage.php
+++ web_ukm/ukm/homepage.php
@@ -87,7 +87,7 @@
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="homepage-ukm/ukm-o.php">UKM-O</a></h4>
+              <h4><a href="homepage-ukm.php">UKM-O</a></h4>
               <p>Unit Kegiatan Mahasiswa Olahraga</p>
             </div>
           </div>
