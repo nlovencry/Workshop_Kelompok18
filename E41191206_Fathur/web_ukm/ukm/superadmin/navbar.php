e30d2a652e76afc82ebc1ed5a19c01228e13b642
Fathur Rohman <fathur.rahman12300@gmail.com>
2020-12-12 12:12:37 +0700

    Project

---
+++ E41191206_Fathur/web_ukm/ukm/superadmin/navbar.php
@@ -0,0 +1,20 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="../../login.php" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    
+    <ul class="navbar-nav ml-auto">
+      <a href="../../login.php">
+      <button class="btn btn-danger">
+        Logout
+      </button>
+    </a>
+  </ul>
+  </nav>
+  <!-- /.navbar -->
