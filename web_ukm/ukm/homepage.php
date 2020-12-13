d73f541acd51d0ed035a8ebab83610055684509c
bungakim <72001593+bungakim@users.noreply.github.com>
2020-12-13 15:46:33 +0700

    Update homepage.php

--- web_ukm/ukm/homepage.php
+++ web_ukm/ukm/homepage.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Sistem Informasi Pendaftaran UKM</title>
+  <title>Sistem Informasi Pendaftaran Unit Kegiatan Mahasiswa</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
