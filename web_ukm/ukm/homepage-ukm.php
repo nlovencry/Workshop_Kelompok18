dca606155c4a36323ef2d2085b476d58c1b680ea
ResaWahyuSaputra <resawahyu.sdi24@gmail.com>
2020-12-13 16:04:10 +0700

    Update homepage-ukm.php

--- web_ukm/ukm/homepage-ukm.php
+++ web_ukm/ukm/homepage-ukm.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Unit Kegiatan Mahasiswa Olahraga</title>
+  <title>UKM Olahraga</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
