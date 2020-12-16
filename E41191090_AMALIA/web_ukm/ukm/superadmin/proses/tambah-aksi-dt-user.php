36ccc2d503c911bc828177e6f50050e85c03da30
Amaliafcn <71832956+Amaliafcn@users.noreply.github.com>
2020-12-16 07:17:01 +0700

    update

---
+++ E41191090_AMALIA/web_ukm/ukm/superadmin/proses/tambah-aksi-dt-user.php
@@ -0,0 +1,21 @@
+<?php
+include '../../koneksi.php';
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM tbl_user WHERE username='$username'";
+$query = mysql_query($sql) or die (mysql_error());
+
+mysqli_query($db,"insert into tb_user values('','$username','$password')");
+if(mysql_num_rows($query) > 0){
+    header("location:../pecah/dt-user.php?pesan=input");
+
+} else {
+
+    $sql = "INSERT INTO tbl_user(username, password, status) VALUES('$username','$password','0')";
+    $query = mysql_query($sql) or die (mysql_error());
+
+    header("location: form_registrasi.php?hasil=sukses");
+}
+
+?>
