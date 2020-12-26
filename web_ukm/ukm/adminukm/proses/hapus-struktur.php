a37c8eec5c585723fe2a04b12075b5ede8fcdccc
bungakim <72001593+bungakim@users.noreply.github.com>
2020-12-26 21:22:30 +0700

    Create hapus-struktur.php

---
+++ web_ukm/ukm/adminukm/proses/hapus-struktur.php
@@ -0,0 +1,8 @@
+<?php 
+	include '../../koneksi.php';
+
+	$id_struktur = $_GET['id_struktur'];
+
+	mysqli_query($db, "DELETE FROM tb_struktur WHERE id_struktur='$id_struktur'");
+	header("location:../pecah/detail-struktur.php");
+?>
